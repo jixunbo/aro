5e4e01eb60c58737b9af706527d5b7812e289d6b
jixunbo <44623656+jixunbo@users.noreply.github.com>
2026-09-01 15:48:17 +0200

    Handle watch refresh with SwiftUI background task

--- aroWatch/AROWatchApp.swift
+++ aroWatch/AROWatchApp.swift
@@ -1,14 +1,21 @@
 import SwiftUI
+import WatchKit
 
 @main
 struct AROWatchApp: App {
-    @WKExtensionDelegateAdaptor(WatchExtensionDelegate.self) private var extensionDelegate
+    @WKApplicationDelegateAdaptor(WatchExtensionDelegate.self) private var applicationDelegate
     @StateObject private var reporter = WatchBatteryService.shared
 
     var body: some Scene {
         WindowGroup {
             WatchContentView()
                 .environmentObject(reporter)
+        }
+        .backgroundTask(.appRefresh(WatchBackgroundRefresh.identifier)) {
+            await MainActor.run {
+                WatchBackgroundRefresh.scheduleNext()
+            }
+            await WatchBatteryService.shared.refreshAndSendAsync()
         }
     }
 }
